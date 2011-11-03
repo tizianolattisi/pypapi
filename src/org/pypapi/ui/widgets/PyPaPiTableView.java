6de26681c1319554470e531c69154d2a03104d92
tiziano <tiziano@axiastudio.it>
2011-11-03 13:59:59 +0100

    PyPaPiTableView placeholder and promotion in author form

---
+++ src/org/pypapi/ui/widgets/PyPaPiTableView.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.pypapi.ui.widgets;
+
+import com.trolltech.qt.core.*;
+import com.trolltech.qt.gui.*;
+
+/**
+ *
+ * @author tiziano
+ *
+ * PyPaPiTableView is a custom widget implementing info, open, add and delete
+ * of rows in the bag list.
+ *
+ */
+public class PyPaPiTableView extends QTableView{
+
+    public PyPaPiTableView(){
+        /*
+         *  init
+         */
+    }
+
+}
