a89bb0f86c12893019adecfc5fee8db2067974c7
tiziano <tiziano@axiastudio.it>
2012-01-10 18:33:25 +0100

    Disables the action in popup when there's no selection.

--- src/org/pypapi/ui/widgets/PyPaPiTableView.java
+++ src/org/pypapi/ui/widgets/PyPaPiTableView.java
@@ -21,6 +21,8 @@ import com.trolltech.qt.gui.*;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.pypapi.GlobalManager;
 import org.pypapi.ui.Form;
 import org.pypapi.ui.TableModel;
@@ -80,20 +82,36 @@ public class PyPaPiTableView extends QTableView{
     }
 
 
-    private void contextMenu(QPoint point) throws NoSuchMethodException, IllegalAccessException, IllegalArgumentException, InvocationTargetException{
+    private void contextMenu(QPoint point){
+        List<QModelIndex> rows = this.selectionModel().selectedRows();
+        Boolean selected = !rows.isEmpty();
+        this.actionInfo.setEnabled(selected);
+        this.actionOpen.setEnabled(selected);
+        this.actionDel.setEnabled(selected);
         QAction action = this.menuPopup.exec(this.mapToGlobal(point));
         if (this.actionOpen.equals(action)){
-            List<QModelIndex> rows = this.selectionModel().selectedRows();
             for (QModelIndex idx: rows){
                 TableModel model = (TableModel) this.model();
                 Object entity = model.getEntityByRow(idx.row());
                 Object relation = GlobalManager.queryRelation(this, "reference");
                 if ( relation != null ){
-                    String name = (String) relation;
-                    String getterName = "get" + name.substring(0,1).toUpperCase() +
-                    name.substring(1);
-                    Method m = entity.getClass().getMethod(getterName);
-                    entity = m.invoke(entity);
+                    try {
+                        String name = (String) relation;
+                        String getterName = "get" + name.substring(0,1).toUpperCase() +
+                        name.substring(1);
+                        Method m = entity.getClass().getMethod(getterName);
+                        entity = m.invoke(entity);
+                    } catch (IllegalAccessException ex) {
+                        Logger.getLogger(PyPaPiTableView.class.getName()).log(Level.SEVERE, null, ex);
+                    } catch (IllegalArgumentException ex) {
+                        Logger.getLogger(PyPaPiTableView.class.getName()).log(Level.SEVERE, null, ex);
+                    } catch (InvocationTargetException ex) {
+                        Logger.getLogger(PyPaPiTableView.class.getName()).log(Level.SEVERE, null, ex);
+                    } catch (NoSuchMethodException ex) {
+                        Logger.getLogger(PyPaPiTableView.class.getName()).log(Level.SEVERE, null, ex);
+                    } catch (SecurityException ex) {
+                        Logger.getLogger(PyPaPiTableView.class.getName()).log(Level.SEVERE, null, ex);
+                    }
                 }
                 Form form = Util.formFromEntity(entity);
                 form.show();
@@ -101,6 +119,17 @@ public class PyPaPiTableView extends QTableView{
         } else if (this.actionAdd.equals(action)){
             // TODO: select and insert new row
         }
+    }
+    
+    @Override
+    protected void enterEvent(QEvent event){
+        //QPoint point = new QPoint(-50, -50);
+        //this.contextMenu(point);
+    }
+
+    @Override
+    protected void leaveEvent(QEvent event){
+        //this.menuPopup.close();
     }
 
 }
