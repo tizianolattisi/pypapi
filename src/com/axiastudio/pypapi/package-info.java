b5ffc820f08c503d35fb7aca420082d27a0de1bd
tiziano <tiziano@192.168.1.122>
2013-01-02 11:29:46 +0100

    Package info.

---
+++ src/com/axiastudio/pypapi/package-info.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (C) 2012 AXIA Studio (http://www.axiastudio.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * PyPaPi Java Desktop Application Framework based on JPA2 persistence provider and Qt toolkit.
+ * 
+ * For more information please visit http://www.pypapi.org
+ * 
+ */
+package com.axiastudio.pypapi;
