24df5efd5d709433d6e458cc8849104c6ff2eefd
tiziano <tiziano@axiastudio.it>
2013-02-25 13:06:21 +0100

    IStreamProvider interface (implementations in Ooops OpenOffice and Cmis Alfresco plugin).

---
+++ src/com/axiastudio/pypapi/IStreamProvider.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.axiastudio.pypapi;
+
+import java.io.InputStream;
+
+/**
+ *
+ * @author AXIA Studio (http://www.axiastudio.com)
+ */
+public interface IStreamProvider {
+    public InputStream getInputStream();
+}
